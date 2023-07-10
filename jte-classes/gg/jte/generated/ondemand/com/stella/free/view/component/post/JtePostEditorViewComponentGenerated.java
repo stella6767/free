3a45f8e33be3f0f8c8fe8588a2261a684e852a7c
kang <stellaalsrb@gmail.com>
2023-07-10 20:10:40 +0900

    [Add] toast editor

---
+++ jte-classes/gg/jte/generated/ondemand/com/stella/free/view/component/post/JtePostEditorViewComponentGenerated.java
@@ -0,0 +1,31 @@
+package gg.jte.generated.ondemand.com.stella.free.view.component.post;
+public final class JtePostEditorViewComponentGenerated {
+	public static final String JTE_NAME = "com/stella/free/view/component/post/PostEditorViewComponent.jte";
+	public static final int[] JTE_LINE_INFO = {42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,42,43,60,63,64,68,69,79,80,82};
+	private static final gg.jte.runtime.BinaryContent BINARY_CONTENT = gg.jte.runtime.BinaryContent.load(JtePostEditorViewComponentGenerated.class, "JtePostEditorViewComponentGenerated.bin", 764,17,585,121,25,163,25,169,5,11);
+	private static final byte[] TEXT_PART_BINARY_0 = BINARY_CONTENT.get(0);
+	private static final byte[] TEXT_PART_BINARY_1 = BINARY_CONTENT.get(1);
+	private static final byte[] TEXT_PART_BINARY_2 = BINARY_CONTENT.get(2);
+	private static final byte[] TEXT_PART_BINARY_3 = BINARY_CONTENT.get(3);
+	private static final byte[] TEXT_PART_BINARY_4 = BINARY_CONTENT.get(4);
+	private static final byte[] TEXT_PART_BINARY_5 = BINARY_CONTENT.get(5);
+	private static final byte[] TEXT_PART_BINARY_6 = BINARY_CONTENT.get(6);
+	private static final byte[] TEXT_PART_BINARY_7 = BINARY_CONTENT.get(7);
+	private static final byte[] TEXT_PART_BINARY_8 = BINARY_CONTENT.get(8);
+	private static final byte[] TEXT_PART_BINARY_9 = BINARY_CONTENT.get(9);
+	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
+		jteOutput.writeBinaryContent(TEXT_PART_BINARY_0);
+		jteOutput.writeBinaryContent(TEXT_PART_BINARY_1);
+		jteOutput.writeBinaryContent(TEXT_PART_BINARY_2);
+		jteOutput.writeBinaryContent(TEXT_PART_BINARY_3);
+		jteOutput.writeBinaryContent(TEXT_PART_BINARY_4);
+		jteOutput.writeBinaryContent(TEXT_PART_BINARY_5);
+		jteOutput.writeBinaryContent(TEXT_PART_BINARY_6);
+		jteOutput.writeBinaryContent(TEXT_PART_BINARY_7);
+		jteOutput.writeBinaryContent(TEXT_PART_BINARY_8);
+		jteOutput.writeBinaryContent(TEXT_PART_BINARY_9);
+	}
+	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
+		render(jteOutput, jteHtmlInterceptor);
+	}
+}
---
+++ src/main/kotlin/com/stella/free/view/component/post/PostEditorViewComponent.kt
@@ -0,0 +1,22 @@
+package com.stella.free.view.component.post
+
+import de.tschuehly.spring.viewcomponent.core.ViewComponent
+import de.tschuehly.spring.viewcomponent.jte.ViewContext
+
+
+@ViewComponent
+class PostEditorViewComponent(
+
+) {
+
+
+
+
+    fun render(): ViewContext {
+
+        return ViewContext(
+
+        )
+    }
+
+}

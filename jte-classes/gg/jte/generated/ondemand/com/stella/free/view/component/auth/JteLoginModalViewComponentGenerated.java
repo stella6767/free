dbde0e700c837389b41774e44086edd96aeb69d3
kang <stellaalsrb@gmail.com>
2023-07-18 19:44:51 +0900

    [Add] editor

---
+++ jte-classes/gg/jte/generated/ondemand/com/stella/free/view/component/auth/JteLoginModalViewComponentGenerated.java
@@ -0,0 +1,20 @@
+package gg.jte.generated.ondemand.com.stella.free.view.component.auth;
+import de.tschuehly.spring.viewcomponent.jte.ViewContext;
+public final class JteLoginModalViewComponentGenerated {
+	public static final String JTE_NAME = "com/stella/free/view/component/auth/LoginModalViewComponent.jte";
+	public static final int[] JTE_LINE_INFO = {0,0,5,5,5,5,5,5,5,5,5,5,8,37,44};
+	private static final gg.jte.runtime.BinaryContent BINARY_CONTENT = gg.jte.runtime.BinaryContent.load(JteLoginModalViewComponentGenerated.class, "JteLoginModalViewComponentGenerated.bin", 4,3,1503,87);
+	private static final byte[] TEXT_PART_BINARY_0 = BINARY_CONTENT.get(0);
+	private static final byte[] TEXT_PART_BINARY_1 = BINARY_CONTENT.get(1);
+	private static final byte[] TEXT_PART_BINARY_2 = BINARY_CONTENT.get(2);
+	private static final byte[] TEXT_PART_BINARY_3 = BINARY_CONTENT.get(3);
+	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor) {
+		jteOutput.writeBinaryContent(TEXT_PART_BINARY_0);
+		jteOutput.writeBinaryContent(TEXT_PART_BINARY_1);
+		jteOutput.writeBinaryContent(TEXT_PART_BINARY_2);
+		jteOutput.writeBinaryContent(TEXT_PART_BINARY_3);
+	}
+	public static void renderMap(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, java.util.Map<String, Object> params) {
+		render(jteOutput, jteHtmlInterceptor);
+	}
+}

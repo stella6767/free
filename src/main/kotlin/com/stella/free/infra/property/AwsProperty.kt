ccbaa7a138690338828695ac155534711d653066
kang <stellaalsrb@gmail.com>
2023-07-20 18:51:01 +0900

    [Add] EnableConfigurationProperties

--- src/main/kotlin/com/stella/free/infra/property/AwsProperty.kt
+++ src/main/kotlin/com/stella/free/infra/property/AwsProperty.kt
@@ -1,12 +1,16 @@
 package com.stella.free.infra.property
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.bind.ConstructorBinding
+
+
 
 
 @ConfigurationProperties(prefix = "cloud.aws")
 class AwsProperty(
     val credentials: Credentials,
     val s3: S3,
+    val region:Region
 ){
 
 
@@ -19,5 +23,10 @@ class AwsProperty(
     data class S3(
         val bucket: String,
         val url: String,
+    )
+
+
+    data class Region(
+        val static: String,
     )
 }

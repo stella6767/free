2e55c8d37a89fea9544a4651585aa86a7e4e6caa
stella6767 <stella@6767>
2023-08-07 19:18:05 +0900

    [Add] openChat complete

--- src/main/kotlin/com/stella/free/core/chat/api/ChatController.kt
+++ src/main/kotlin/com/stella/free/core/chat/api/ChatController.kt
@@ -1,6 +1,7 @@
 package com.stella.free.core.chat.api
 
-import com.stella.free.core.chat.dto.ChatDto
+import com.stella.free.core.chat.dto.ChatResDto
+import com.stella.free.core.chat.dto.ChatSendDto
 import com.stella.free.global.util.logger
 
 
@@ -19,51 +20,46 @@ class ChatController(
 
     @MessageMapping("/chat.sendMessage")
     @SendTo("/topic/public")
-    fun sendMessage(@Payload chatDto: ChatDto): String {
-        log.info("service: $chatDto")
-        return chatBoxTemplate(chatDto)
+    fun sendMessage(@Payload chatSendDto: ChatSendDto): ChatResDto {
+        log.info("service: $chatSendDto")
+
+        return ChatResDto(
+            sender = chatSendDto.sender,
+            html = chatBoxTemplate(chatSendDto)
+        )
     }
 
 
     @MessageMapping("/chat.addUser")
     @SendTo("/topic/public")
-    fun addUser(@Payload chatDto: ChatDto, headerAccessor: SimpMessageHeaderAccessor): String {
-
-        log.info("요청옴: $chatDto ,  $headerAccessor")
-
-        return chatBoxTemplate(chatDto)
+    fun addUser(@Payload chatSendDto: ChatSendDto, headerAccessor: SimpMessageHeaderAccessor): ChatResDto {
+        log.info("요청옴: $chatSendDto ,  $headerAccessor")
+        return ChatResDto(
+            sender = chatSendDto.sender,
+            html = chatBoxTemplate(chatSendDto)
+        )
     }
 
 
-    private fun chatBoxTemplate(chatDto: ChatDto): String {
-        return """
-            <div class="chat-message">
-                <div class="flex items-end">              
-                    <div class="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-2 items-start">
-                        <div>                    
-                            <span class="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">
-                                ${chatDto.message}
-                            </span>
+
+    private fun chatBoxTemplate(chatSendDto: ChatSendDto): String {
+        return """      
+            <div class="chat-message mt-2">
+                <div id="chat-box-comment"
+                        class="flex">
+                    <div 
+                            class="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1">
+                        <div>
+                                    <span id="chat-message-box" class="px-4 py-2 rounded-lg inline-block rounded-bl-none bg-gray-300 text-gray-600">
+                                        ${chatSendDto.message}
+                                    </span>
+                        </div>
+                        <div class="max-w-xs mx-2 text-xs font-bold">
+                            ${chatSendDto.sender}
                         </div>
                     </div>
-                    <div class="max-w-xs mx-2 stext-xs font-bold">
-                        ${chatDto.sender}                   
-                    </div>
                 </div>
-            </div>
-    
-    
-            <div class="chat-message">
-                <div class="flex items-end justify-end">
-                    <div class="flex flex-col space-y-2 text-xs max-w-xs mx-2 order-1 items-end">
-                        <div><span class="px-4 py-2 rounded-lg inline-block rounded-br-none bg-blue-600 text-white ">Any updates on this issue? I'm getting the same error when trying to install devtools. Thanks</span>
-                        </div>
-                    </div>
-                    <div class="max-w-xs mx-2 stext-xs font-bold">
-                        username
-                    </div>
-                </div>
-            </div>                       
+            </div>          
             """.trimIndent()
     }
 
--- src/main/kotlin/com/stella/free/web/page/chat/ChatViewComponent.kt
+++ src/main/kotlin/com/stella/free/web/page/chat/ChatViewComponent.kt
@@ -1,13 +1,13 @@
 package com.stella.free.web.page.chat
 
-import com.stella.free.web.component.chat.ChatBoxViewComponent
+
 import de.tschuehly.spring.viewcomponent.core.ViewComponent
 import de.tschuehly.spring.viewcomponent.jte.ViewContext
 
 
 @ViewComponent
 class ChatViewComponent(
-    private val chatBoxViewComponent: ChatBoxViewComponent,
+
 ) {
 
     fun render(): ViewContext {

ef77c9d2db9e0b7b284199d34ab9b030badf827b
stella6767 <stella@6767>
2023-09-20 13:58:49 +0900

    [Fix] deploy sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -32,4 +32,6 @@ echo "========================"
 echo "Jar execute"
 echo "========================"
 
-sudo nohup /usr/bin/java -Djasypt.encryptor.password=1234 -jar ~/cicd/*.jar --spring.profiles.active=prod > ~/cicd/nohup.log 2>&1 &
+sudo nohup /usr/bin/java -jar ~/cicd/*.jar --spring.profiles.active=prod > ~/cicd/nohup.log 2>&1 &
+
+# sudo nohup /usr/bin/java -Djasypt.encryptor.password=1234 -jar ~/cicd/*.jar --spring.profiles.active=prod > ~/cicd/nohup.log 2>&1 &

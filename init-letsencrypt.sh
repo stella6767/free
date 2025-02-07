3900c4ad45632de06838c61b6987b24dd41e1b65
stella6767 <stella@6767>
2025-02-07 22:30:16 +0900

    [fix] domain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=www.freeapp.life
+domains=www.freeapp.me
 rsa_key_size=4096
 data_path="./certbot"
 email="" # Adding a valid address is strongly recommended

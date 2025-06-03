333acfb23fb1bb3ecc3980150f86c620b37ccdef
stella6767 <stella@6767>
2025-06-04 01:58:00 +0900

    [fix] deploy-docker.sh9

--- scripts/deploy-docker.sh
+++ scripts/deploy-docker.sh
@@ -7,6 +7,8 @@ echo "========================"
 
 cd "$HOME/cicd/me"
 
+echo $PWD
+
 #openssl req -x509 -nodes -newkey rsa:2048 -keyout key.pem -out cert.pem -sha256 -days 365 \
 #    -subj "/C=GB/ST=London/L=London/O=Alros/OU=IT Department/CN=localhost"
 

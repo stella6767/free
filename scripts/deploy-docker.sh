a51c5e8627dca50c4339c7dc95281641252ed3bb
stella6767 <stella@6767>
2025-06-04 01:25:33 +0900

    [fix] deploy-docker.sh3

--- scripts/deploy-docker.sh
+++ scripts/deploy-docker.sh
@@ -5,7 +5,7 @@ echo "========================"
 echo "Path move"
 echo "========================"
 
-cd "$HOME/cicd/me"
+cd /home/ubuntu/cicd/me
 
 #openssl req -x509 -nodes -newkey rsa:2048 -keyout key.pem -out cert.pem -sha256 -days 365 \
 #    -subj "/C=GB/ST=London/L=London/O=Alros/OU=IT Department/CN=localhost"
